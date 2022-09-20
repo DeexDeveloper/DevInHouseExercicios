d25fc3e62cfe1f434a2d45d5c609d4bad5bc78ce
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-20 14:54:01 -0300

    exercicio 1 da segunda semana

--- M01S02/Ex1.java
+++ M01S02/Ex1.java
@@ -1,3 +1,14 @@
+/*
+ Implemente um código copiando o array abaixo, e depois imprima no console o valor dos seguintes elementos:
+
+int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+a) Imprima o valor do primeiro elemento;
+b) Imprima o valor do último elemento;
+c) Imprima o valor do terceiro elemento;
+ */
+
+
 package M01S02;
 
 public class Ex1 {
