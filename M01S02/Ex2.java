63d4f7c05d72b2752fe96c79ef4214fe12a09f8e
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-20 15:33:30 -0300

    adicionando alugmas coisas no ex 2

---
+++ M01S02/Ex2.java
@@ -0,0 +1,26 @@
+/*
+Implemente um código copiando o array abaixo, e depois imprima no console o valor dos seguintes elementos:
+
+double[][] notas = {
+    {10, 8.5, 5.5, 9.5},
+    {8.5, 7, 7.5, 6},
+    {8, 9, 10, 7.5},
+    {9, 9.5, 10, 8}
+};
+a) Imprima o valor da nota do primeiro elemento do terceiro array de notas.
+b) Imprima o valor da nota do segundo elemento do quarto array de notas.
+c) Imprima o valor da soma das quatro primeiras notas de cada array.
+*/
+package M01S02;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        double[][] notas = {
+            {10, 8.5, 5.5, 9.5},
+            {8.5, 7, 7.5, 6},
+            {8, 9, 10, 7.5},
+            {9, 9.5, 10, 8}
+        };
+
+    }
+}
