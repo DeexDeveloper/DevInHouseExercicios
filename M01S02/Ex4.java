cc22ac2ca46f7fe65c8dd455062d3f763aa91bf5
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-21 15:58:11 -0300

    inclusao do quarto exercicio

---
+++ M01S02/Ex4.java
@@ -0,0 +1,23 @@
+/*
+Mike tem um nro da sorte: 25.
+Ele conseguiu uma listagem do histórico dos resultados da loteria conforme arrays abaixo.
+Escreva um código para percorrer os elementos de nros sorteados e contabilizar quantas vezes 
+o nro da sorte dele foi sorteado.
+* Dica: Usar um for dentro do outro para percorrer os arrays internos.
+```
+int sorteados[][] = {
+    { 1, 44,  6,  2, 45, 60},
+    {10, 21, 55, 25, 34, 44},
+    { 8, 18, 28, 29, 55, 59},
+    {60, 25, 11, 34,  6,  9},
+    {55, 43, 25, 12,  7, 11}
+};
+```
+ */
+package M01S02;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        
+    }
+}
