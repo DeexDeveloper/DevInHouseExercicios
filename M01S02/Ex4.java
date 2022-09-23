58d7a19a14e5bf802baffd0506f3da0d31e9156b
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-23 16:57:13 -0300

    resolução exercício 4

--- M01S02/Ex4.java
+++ M01S02/Ex4.java
@@ -18,6 +18,21 @@ package M01S02;
 
 public class Ex4 {
     public static void main(String[] args) {
-        
+        int sorteados[][] = {
+            { 1, 44,  6,  2, 45, 60},
+            {10, 21, 55, 25, 34, 44},
+            { 8, 18, 28, 29, 55, 59},
+            {60, 25, 11, 34,  6,  9},
+            {55, 43, 25, 12,  7, 11}
+        };
+        int somaSorte = 0;
+        for  (int linha = 0; linha<sorteados.length; linha++){
+            for (int coluna = 0; coluna<sorteados[linha].length; coluna++){
+                if (sorteados[linha][coluna]==25){
+                    somaSorte++;
+                }
+            }
+       }
+       System.out.println("O numero da sorte de Mike saiu "+somaSorte+" vezes.");
     }
 }
