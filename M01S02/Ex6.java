c770f08debb6d00accfd01b3e50015801156b142
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-28 17:56:24 -0300

    arrumando ex 6

--- M01S02/Ex6.java
+++ M01S02/Ex6.java
@@ -8,14 +8,16 @@ Percorra a lista e gere uma nova com o triplo do valor de cada elemento:
 
 package M01S02;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Ex6 {
     public static void main(String[] args) {
         List<Integer> lista = Arrays.asList(1,2,3,4);
-        List<Integer> novaLista = Arrays.asList();
+        List<Integer> novaLista = new ArrayList<>();
         for (Integer i : lista){
-            novaLista.add(i);
+            novaLista.add(i*3);
         }
         for (Integer i : novaLista){
             System.out.println(i);
--- M01S03/Ex1.java
+++ M01S03/Ex1.java
@@ -1,3 +1,8 @@
+/*
+Crie uma classe 'Funcionario' para representar um empregado de uma empresa, contendo os atributos cpf, nome completo e salário.
+Implemente também um método 'promover' que recebe como parâmetro um percentual (float), devendo o salário ser aumentado pelo percentual indicado no argumento do método.
+Crie e utilize um objeto desta classe para testar a implementação.
+*/
 package M01S03;
 
 public class Ex1 {
