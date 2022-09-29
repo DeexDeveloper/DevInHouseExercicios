ef24d50712682a262a9e3c9f824e0b71aecf72c4
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-29 14:46:55 -0300

    palindrome

--- M01S02/Ex10.java
+++ M01S02/Ex10.java
@@ -6,10 +6,7 @@ Ou seja, é uma palavra que quando lida de trás pra frente tem o mesmo signific
 Implemente um código com o que aprendemos até agora que retorne se determinada palavra é um palíndromo.
 */
 package M01S02;
-
 import java.util.Scanner;
-
-import javax.swing.text.PlainDocument;
 
 public class Ex10 {
     public static void main(String[] args) {
