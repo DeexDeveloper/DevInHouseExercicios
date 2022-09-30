e2e162b4d49a65abc2427e11eddc29bd2a4c6b57
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-09-30 12:01:44 -0300

    testando o branch

--- M01S03/ex1/Ex1.java
+++ M01S03/ex1/Ex1.java
@@ -3,7 +3,7 @@ Crie uma classe 'Funcionario' para representar um empregado de uma empresa, cont
 Implemente também um método 'promover' que recebe como parâmetro um percentual (float), devendo o salário ser aumentado pelo percentual indicado no argumento do método.
 Crie e utilize um objeto desta classe para testar a implementação.
 */
-package M01S03;
+package M01S03.ex1;
 
 public class Ex1 {
     public static void main(String[] args) {
---
+++ M01S03/ex1/Funcionario.java
@@ -0,0 +1,8 @@
+package M01S03.ex1;
+
+public class Funcionario {
+    private Long cpf;
+    private Float salario;
+    private String nome;
+   
+}
