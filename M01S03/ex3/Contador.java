1a017b81271e43d7da3b3445afce6d9ca3909b7f
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-10-06 13:37:37 -0300

    ex4

---
+++ M01S03/ex3/Contador.java
@@ -0,0 +1,5 @@
+package M01S03.ex3;
+
+public class Contador {
+    
+}
---
+++ M01S03/ex3/Ex3.java
@@ -0,0 +1,13 @@
+/*
+a) Crie uma classe 'Contador'.
+b) Crie um método estático (static) 'contarPalavras' que recebe uma frase (String) como argumento. 
+c) Implemente o método imprimindo no console o número de palavras da frase recebida como parâmetro.
+d) Crie um método main em outra classe para testar a implementação.
+Dicas:
+- Usar método split() da String ou classe StringTokenizer.
+*/
+package M01S03.ex3;
+
+public class Ex3 {
+    
+}
