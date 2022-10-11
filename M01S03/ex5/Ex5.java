dbea9a9deac76e80dfa16b3af6305fa57be2cdeb
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-10-11 11:01:20 -0300

    ex5

--- M01S03/ex5/Ex5.java
+++ M01S03/ex5/Ex5.java
@@ -25,6 +25,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ex5 {
@@ -33,7 +36,26 @@ public class Ex5 {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        Path arquivo = Paths.get(.. megasena.txt)
-        List<String> linhas = Files.readAllLines(megasena.txt);
+        Path arquivo = Paths.get("megasena.txt");
+        List<String> lista = Files.readAllLines(arquivo);
+        List<Concurso> concursos = new ArrayList<>();
+        DateTimeFormatter formatador =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        for (String linha : lista){
+            String partes[] = linha.split(",");
+            Integer numero = Integer.valueOf(partes[0]);
+            String dataStr = partes[1];
+            LocalDate data = LocalDate.parse(dataStr, formatador);
+            int sorteados[] = new int[6];
+            for (int i=2; i<=7; i++){
+                sorteados[i-2] = Integer.parseInt(partes[i]);
+            }
+            Concurso concurso = new Concurso(numero,data,sorteados);
+            concursos.add(concurso);
+
+        }
+        for (Concurso imprimir :concursos){
+            System.out.println(imprimir);
+        }              
     }
 }
