24b32b6bdebfefa170ac96394fa13be5809fd213
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-10-21 19:07:23 -0300

    outra atualização

---
+++ M01S04/superHerois/org/devinhouse/superherois/cli/Display.java
@@ -0,0 +1,5 @@
+package M01S04.superHerois.org.devinhouse.superherois.cli;
+
+public class Display {
+
+}
---
+++ M01S04/superHerois/org/devinhouse/superherois/repository/PersonagemRepository.java
@@ -0,0 +1,10 @@
+package M01S04.superHerois.org.devinhouse.superherois.repository;
+
+import M01S04.superHerois.org.devinhouse.superherois.model.Personagem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonagemRepository {
+    private List<Personagem> personagens = new ArrayList<>();
+}
