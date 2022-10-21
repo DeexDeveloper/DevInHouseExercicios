8339b114c99265e91669ce964fcd6bb4949b258e
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-10-21 11:48:08 -0300

    ex 2

---
+++ M01S04/ex2/Funcionario.java
@@ -0,0 +1,8 @@
+package M01S04.ex1.ex2;
+/*a) Crie uma classe 'Funcionario' com o método público "obterCargo" que retorne uma String com o texto "Sou Funcionário".*/
+
+public class Funcionario {
+    public String obterCargo(){
+        return "Sou Funcionário.";
+    }
+}
